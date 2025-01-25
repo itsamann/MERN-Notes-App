816f9ce268d64f8541e4210e32ae70982f97c4bd
Aman Sharma <amansha2047@gmail.com>
2025-01-25 09:09:41 -0500

    Created CreateProductPage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "./components/ui/provider";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "./components/ui/toaster.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider>
+        <Toaster />
         <App />
       </Provider>
     </BrowserRouter>
--- client/src/pages/CreatePage.jsx
+++ client/src/pages/CreatePage.jsx
@@ -1,7 +1,98 @@
-import React from "react";
+import {
+  Button,
+  Heading,
+  Box,
+  Container,
+  VStack,
+  Input,
+} from "@chakra-ui/react";
+import { toaster } from "../components/ui/toaster";
+import { React, useState } from "react";
+import { useColorModeValue } from "../components/ui/color-mode";
+import { useProductStore } from "../store/product";
 
 const CreatePage = () => {
-  return <div>CreatePage</div>;
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+
+  const { createProduct } = useProductStore();
+
+  const handleAddProduct = async () => {
+    const { success, message } = await createProduct(newProduct);
+    if (!success) {
+      toaster.error({
+        title: "Error",
+        description: message,
+        status: "error",
+        isCloseble: true,
+      });
+    } else {
+      toaster.success({
+        title: "Success",
+        description: message,
+        status: "success",
+        isCloseble: true,
+      });
+      setNewProduct({ name: "", price: "", image: "" });
+    }
+  };
+
+  return (
+    <Container maxW={"container.sm"}>
+      <VStack spacing={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+          Create a new product
+        </Heading>
+
+        <Box
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+        >
+          <VStack spacing={4}>
+            <Input
+              placeholder="Product Name"
+              name="name"
+              type="text"
+              value={newProduct.name}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
+            />
+
+            <Input
+              placeholder="Product Price"
+              name="price"
+              type="number"
+              value={newProduct.price}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
+            />
+
+            <Input
+              placeholder="Image URL"
+              name="image"
+              type="text"
+              value={newProduct.image}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
+            />
+
+            <Button colorScheme={"blue"} onClick={handleAddProduct} w="full">
+              Create Product
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
+  );
 };
 
 export default CreatePage;
